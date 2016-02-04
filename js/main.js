9a03ac12793a9fa4c7e937168c52f5e9276dc1cd
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2016-02-04 13:55:15 -0800

    feat: Update cover; Add urls to links

--- js/main.js
+++ js/main.js
@@ -22,12 +22,12 @@ function Section(name,organizations) {
 }
 
 var contacts = [
-  new Contact("(919) 928-7235","fa-phone-square",""),
-  new Contact("cholmgreen@gmail.com","fa-envelope",""),
-  new Contact("icasperzen","fa-github",""),
-  new Contact("casper.holmgreen","fa-facebook-square",""),
-  new Contact("casper.holmgreen","fa-skype",""),
-  new Contact("casper.holmgreen","fa-linkedin-square")
+  new Contact("(919) 928-7235","fa-phone-square","tel:9199287235"),
+  new Contact("cholmgreen@gmail.com","fa-envelope","mailto:cholmgreen@gmail.com"),
+  new Contact("icasperzen","fa-github","https://github.com/icasperzen"),
+  new Contact("casper.holmgreen","fa-facebook-square","https://www.facebook.com/casper.holmgreen"),
+  new Contact("casper.holmgreen","fa-skype","skype:casper.holmgreen?add"),
+  new Contact("casper.holmgreen","fa-linkedin-square","https://www.linkedin.com/in/casperholmgreen")
 ]
 
 var work = new Section("work", [
@@ -40,21 +40,21 @@ var work = new Section("work", [
       "<li>graded 4 assignments per student each week for over 200 students</li>" +
       "<li>voted Best Instructor 2009 of 15 instructors by the student body</li></ul>")
   ], "https://www.ncsu.edu/"),
+  new Organization("Other", [
+    new Position("Private Tutor", "2010-2011", "")
+  ]),
   new Organization("Transoft International, Inc.", [
     new Position("Junior System Administrator","2005-2007", "")
   ], "http://www.ncr.com/"),
   new Organization("Office Max", [
     new Position("Lead Sales Associate","2004-2007","")
-  ], "http://www.officedepot.com/"),
-  new Organization("Other", [
-    new Position("Private Tutor", "2010-2011", "")
-  ])
+  ], "http://www.officedepot.com/")
 ]);
 
 var edu = new Section("education", [
   new Organization("University of Copenhagen", [
     new Position("M.Sc. - Computer Science (Programming Languages)","2013-2015",
-      "Thesis Topic: Differential Privacy with Dependent Types")
+      "Thesis: Differential Privacy with Dependent Types")
   ], "http://www.ku.dk/english/"),
   new Organization("North Carolina State University", [
     new Position("B.Sc. - Nuclear Engineering","2007-2012",""),
@@ -64,10 +64,10 @@ var edu = new Section("education", [
 
 var online = new Section("online", [
   new Organization("Udacity", [
-    new Position("Frontend Developer Nanodegree","2016","I learned how to make this website.")
+    new Position("Frontend Developer Nanodegree","2016","")
   ], "https://www.udacity.com/"),
   new Organization("edX", [
-    new Position("Software as a Service","2012","I learned about SAAS.")
+    new Position("Software as a Service","2012","")
   ], "https://www.edx.org/"),
   new Organization("Oak Ridge National Laboratory",[
     new Position("Non-destructive Assay (NDA) Applications for International Safeguards","2012")
@@ -75,22 +75,18 @@ var online = new Section("online", [
 ]);
 
 var extra = new Section("extra-curricular", [
+  new Organization("Information Assurance Student Group (IASG)", [
+    new Position("NCSU Chapter Vice President","2012")
+  ], "https://getinvolved.ncsu.edu/organization/IASG/"),
   new Organization("Institute of Nuclear Materials Management (INMM)", [
     new Position("NCSU Chapter Communications Officer","2012")
   ], "https://www.inmm.org/"),
   new Organization("American Nuclear Society (ANS)", [
     new Position("NCSU Chapter Member","2012")
-  ], "http://www.ans.org/"),
-  new Organization("Information Assurance Student Group (IASG)", [
-    new Position("NCSU Chapter Vice President","2012")
-  ], "https://getinvolved.ncsu.edu/organization/IASG/")
+  ], "http://www.ans.org/")
 ]);
 
 var pubs = new Section("publications", [
-  new Organization("ANS Student Conference 2012", [
-    new Position("Role of Secondary Electron Emission in Shaped Electrodes with Gaseous Glow Discharges","2012","C. Ratzlaff, C. Holmgreen, E. Rowshanaee<br>" +
-      "American Nuclear Society Student Conference, 2012, April 12-15")
-  ], "http://students.ans.org/student-conferences"),
   new Organization("Agile2013 (Nashville)", [
     new Position("Proposing Regulatory-Driven Automated Test Suites","2013","P. Morrison, C. Holmgreen, A.K. Massey, L. Williams<br>" +
       "Agile Conference (AGILE), 2013, 11-21")
@@ -98,7 +94,11 @@ var pubs = new Section("publications", [
   new Organization("Information and Software Technology (Elsevier)", [
     new Position("A Comparison of the Efficiency and Effectiveness of Vulnerability Discovery Techniques","2013", "A. Austin, C. Holmgreen, L. Williams<br>" +
       "Information and Software Technology 55 (7), 1279-1288")
-  ])
+  ]),
+  new Organization("ANS Student Conference 2012", [
+    new Position("Role of Secondary Electron Emission in Shaped Electrodes with Gaseous Glow Discharges","2012","C. Ratzlaff, C. Holmgreen, E. Rowshanaee<br>" +
+      "American Nuclear Society Student Conference, 2012, April 12-15")
+  ], "http://students.ans.org/student-conferences")
 ]);
 
 var tContact = _.template($("script.tContact").html());
